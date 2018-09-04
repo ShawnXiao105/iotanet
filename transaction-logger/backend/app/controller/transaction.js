340c28ceaec73db0887027c613a2b185cbbd1516
ygjack <ygjack414@hotmail.com>
2018-09-04 13:23:18 +0800

    add leancloud

--- transaction-logger/backend/app/controller/transaction.js
+++ transaction-logger/backend/app/controller/transaction.js
@@ -1,7 +1,10 @@
 'use strict'
 const Controller = require('egg').Controller
-const randomString = require('randomstring')
+const AV = require('leancloud-storage')
+const Transaction = AV.Object.extend('Transaction')
 
+/*
+const randomString = require('randomstring')
 const generateData = () => {
   return {
     token: randomString.generate(90),
@@ -9,22 +12,99 @@ const generateData = () => {
     createdAt: new Date()
   }
 }
+*/
 
 class TransactionController extends Controller {
+
+  async index () {
+    const { deviceId, deviceType, messageType, page = 1, pageSize = 20 } = this.ctx.query
+    const query = new AV.Query('Transaction')
+
+    deviceId && query.equalTo('deviceId', deviceId)
+    deviceType && query.equalTo('deviceType', deviceType)
+    messageType && query.equalTo('messageType', messageType)
+
+    const total = await query.count()
+    query.limit(pageSize)
+    query.skip(pageSize * (page - 1))
+
+    const list = await query.find()
+    
+    this.ctx.response.body = {
+      status: 0,
+      data: {
+        list,
+        meta: {
+          total,
+          totalPage: Math.ceil(total / pageSize),
+          page,
+          pageSize
+        }
+      }
+    }
+  }
+
+  async show () {
+    const query = new AV.Query('Transaction')
+    try {
+      const trx = await query.get(this.ctx.params.id)
+      this.ctx.response.body = {
+        status: 0,
+        data: trx
+      }
+    } catch (e) {
+      if (e.code === 101) {
+        this.ctx.response.body = {
+          status: 0,
+          data: null
+        }
+      } else {
+        this.ctx.response.body = {
+          status: -1,
+          error: { code: e.code, message: e.message }
+        }
+      }
+    }
+  }
+
   /**
    * POST /transaction
    * called by device with transaction log details,
    * and deliver the log to transaction logger web page via socket
    */
   async create () {
-    console.log(this.ctx.request.body)
-    // broadcast the transaction data to all connected socket clients
-    await this.app.io.emit('transaction', generateData())
+    const { body } = this.ctx.request
+    const trx = new Transaction()
+    let error
 
-    // TODO emit('transaction-<type>')
+    ;['deviceId', 'deviceType', 'messageType'].forEach(field => {
+      if (body[field]) {
+        trx.set(field, body[field])
+      } else {
+        error = { code: 1, message: `${field} is empty` }
+      }
+    })
 
-    // reply nothing in response
-    this.ctx.body = 'ok'
+    if (error) {
+      this.ctx.body = {
+        status: -1,
+        error
+      }
+      return
+    }
+
+    try {
+      await trx.save()
+      // broadcast the transaction data to all connected socket clients
+      await this.app.io.emit('transaction', trx)
+      // reply nothing in response
+      this.ctx.body = trx
+    } catch (e) {
+      this.ctx.response.body = {
+        status: -1,
+        error: { code: e.code, message: e.message }
+      }
+    }
   }
 }
 
--- transaction-logger/backend/app/router.js
+++ transaction-logger/backend/app/router.js
@@ -1,5 +1,14 @@
 'use strict'
 
+const APP_ID = '3Fhv8rFwcN2IX6JHDxmomozU-gzGzoHsz'
+const APP_KEY = 'EUNG1BzfzHjL6UAb6YY2oyGD'
+const AV = require('leancloud-storage')
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
 /**
  * @param {Egg.Application} app - egg application
  */
@@ -7,7 +16,7 @@ module.exports = app => {
   const { router, controller } = app
   
   router.get('/', controller.home.index)
-  router.post('/api/transaction', controller.transaction.create)
+  router.resources('transaction', '/api/transaction', controller.transaction)
 
   // socket.io
   /* app.io.route('PING', app.io.controller.index.ping)
